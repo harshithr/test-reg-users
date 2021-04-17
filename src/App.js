2beeabed5c4c4bd56dd07eb9ceea2b85e5eca2be
harshithr <harshithr.pro@gmail.com>
2021-04-17 20:13:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Register from './components/register';
+import Users from './components/users';
 
 function App() {
+  const [ conditionalRender, setConditionalRender ] = useState(undefined);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h2>My Assignment</h2>
+        <p>Click on the button to view the particular assignment.</p>
+        <Button onClick={() => setConditionalRender("register")}>
+          Assignment 1 Register
+        </Button>
+        <Button onClick={() => setConditionalRender("users")} style={{ marginLeft: '30px' }}>
+          Assignment 2 Users
+        </Button>
+
+        {(conditionalRender === "register") ? <Register /> : (conditionalRender === "users") ? <Users /> : null}
+      </Container>
     </div>
   );
 }
